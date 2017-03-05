d39cbbdc015498d15394067bc70d192cbcb924d1
girltech <mariacutipa.gonzales@gmail.com>
2017-03-04 21:38:15 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-
-window.addEventListener("click",function(){
-	validateForm();
-});
 function validateForm(){
-	/* Escribe tú código aquí */
-
+/* Escribe tú código aquí */
 var name = document.getElementById("name").value;
 var lastname = document.getElementById("lastname").value;
 var mail = document.getElementById("input-email").value;
 var pwd = document.getElementById("input-password").value;
 var select = document.getElementById("select");
-
-
+	
 //RegExpresion Mail 
 regxmail = /\w+@\w+\.+[a-z]/;
 regxInput = /^[a-z][a-z]*/;
 
 //validando valor nullo - vacio - [A-Z] para name
-if(name === null && name.length === 0 && regxInput.test(name)) && !name.charAt(0).toUpperCase(){
+if(name.length == 0 || !name.charAt(0).toUpperCase() || !regxInput.test(name) ){
 	alert("Ingrese Nombre no valido");
 	return false;
 }
 
 //validando valor nullo - vacio - [A-Z] para name
-else if(lastname === null && lastname.length === 0 && regxInput.test(lastname) && !lastname.charAt(0).toUpperCase()){
+else if(lastname.length == 0 || !regxInput.test(lastname) || !lastname.charAt(0).toUpperCase()){
 	alert("Ingrese Apellido no valido");
 	return false;
 }
@@ -35,7 +29,7 @@ else if(!regxmail.test(mail)){
 }
 
 //validando password
-else if (pwd.value == "password" && pwd.value == "123456" && pwd.value == "098754" && pwd.length > 6) {
+else if (pwd.value == "password" || pwd.value == "123456" || pwd.value == "098754" || pwd.length > 6) {
 	alert("Ingrese una contraseña valida");
 	return false;
 }
@@ -45,6 +39,4 @@ else if (select.value == "0"){
 	alert("Seleccione una Bici");
 	return false;
 }
-
-
 }
